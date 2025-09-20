a3371ca52475bac25c8c0a0290cb653259e6a666
Leandro Piazza <lean-developer@proton.me>
2025-09-20 18:47:42 -0300

    Add Defer triggers and view transitions API

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-
   {
     path: 'dashboard',
     loadComponent: () => import('./dashboard/dashboard.component'),
@@ -9,22 +8,28 @@ export const routes: Routes = [
       {
         path: 'change-detection',
         title: 'Change Detection',
-        loadComponent: () => import('./dashboard/pages/change-detection/change-detection.component'),
+        loadComponent: () =>
+          import(
+            './dashboard/pages/change-detection/change-detection.component'
+          ),
       },
       {
         path: 'control-flow',
         title: 'Control Flow',
-        loadComponent: () => import('./dashboard/pages/control-flow/control-flow.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/control-flow/control-flow.component'),
       },
       {
         path: 'defer-options',
         title: 'Defer Options',
-        loadComponent: () => import('./dashboard/pages/defer-options/defer-options.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/defer-options/defer-options.component'),
       },
       {
         path: 'defer-views',
         title: 'Defer Views',
-        loadComponent: () => import('./dashboard/pages/defer-views/defer-views.component'),
+        loadComponent: () =>
+          import('./dashboard/pages/defer-views/defer-views.component'),
       },
       {
         path: 'user/:id',
@@ -37,21 +42,31 @@ export const routes: Routes = [
         loadComponent: () => import('./dashboard/pages/users/users.component'),
       },
       {
-        path: 'view-transition',
-        title: 'View Transition',
-        loadComponent: () => import('./dashboard/pages/view-transition/view-transition.component'),
+        path: 'view-transition-1',
+        title: 'View Transition 1',
+        loadComponent: () =>
+          import(
+            './dashboard/pages/view-transition/view-transition1.component'
+          ),
       },
       {
-        path:'', redirectTo: 'control-flow', pathMatch: 'full',
-      }
-    ]
+        path: 'view-transition-2',
+        title: 'View Transition 2',
+        loadComponent: () =>
+          import(
+            './dashboard/pages/view-transition/view-transition2.component'
+          ),
+      },
+      {
+        path: '',
+        redirectTo: 'control-flow',
+        pathMatch: 'full',
+      },
+    ],
   },
   {
     path: '',
     redirectTo: '/dashboard',
-    pathMatch: 'full'
-  }
-
-
-
+    pathMatch: 'full',
+  },
 ];
--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <h1>Hola Mundo</h1>
-  `
+  template: ` <section [ngClass]="['w-full h-[600px]', cssClass]"></section> `,
 })
 export class HeavyLoadersSlowComponent {
+  @Input({ required: true }) cssClass!: string;
 
+  constructor() {
+    console.log('HeavyLoadersSlowComponent loaded');
+    const start: any = new Date();
+    while (Date.now() - start < 3000) {}
+    console.log('Cargado ...');
+  }
 }
